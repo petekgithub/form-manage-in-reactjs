9d6cfd533ee00f3d18e42b081f6512ebd2f3c192
Petek Savaş Hamitbeyli <38165351+petekgithub@users.noreply.github.com>
2022-04-27 16:04:50 +0300

    handleSubmit && handleChange

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import { Formik } from 'formik';
+
+function App() {
+  return (
+    <div className="App">
+     <Formik
+      initialValues=
+      {{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({handleSubmit, handleChange}) => (
+        <form onSubmit={handleSubmit}>
+
+        <label htmlFor="firstName">First Name</label>
+        <input name='firstName' onChange={handleChange}/>
+
+        <br />
+        <br />
+        <label htmlFor="lastName">Last Name</label>
+        <input name='lastName' onChange={handleChange}/>
+
+        <br />
+        <br />
+        <label htmlFor="email">Email</label>
+        <input name='email' onChange={handleChange}/>
+
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+        )
+      }
+      </Formik>
+    </div>
+  );
+}
+
+export default App;
